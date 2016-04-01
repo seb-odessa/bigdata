6eadea0164a2904c97ac33edc80769ac8c116ccc
seb-odessa <seb@ukr.net>
2016-04-02 00:15:42 +0300

    get method implemented

--- hdfs/src/command.java
+++ hdfs/src/command.java
@@ -7,18 +7,26 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class command {
-
-    private static void AssertNotNull(Object arg) {
-	if (arg == null) {
-	    throw new IllegalArgumentException("argument is null.");
-	}
+  private static void AssertNotNull(Object arg) {
+    if (arg == null) {
+      throw new IllegalArgumentException("argument is null.");
     }
+  }
 
-    public static void put(String ifile, String ofile) throws IllegalArgumentException, IOException {
-	AssertNotNull(ifile);
-	AssertNotNull(ofile);
-	Configuration conf = new Configuration();
-	FileSystem.get(conf).copyFromLocalFile(new Path(ifile), new Path(ofile));
-    }
+  public static void put(String ifile, String ofile)
+  throws IllegalArgumentException,  IOException {
+    Configuration conf = new Configuration();
+    Path ipath = new Path(ifile);
+    Path opath = new Path(ofile);
+    opath.getFileSystem(conf).copyFromLocalFile(ipath, opath);
+  }
+
+  public static void get(String ifile, String ofile)
+  throws IllegalArgumentException,  IOException {
+    Configuration conf = new Configuration();
+    Path ipath = new Path(ifile);
+    Path opath = new Path(ofile);
+    ipath.getFileSystem(conf).copyToLocalFile(ipath, opath);
+  }
 
 }
--- hdfs/src/hdfs.java
+++ hdfs/src/hdfs.java
@@ -7,34 +7,40 @@ import hdfs.command;
 
 public class hdfs {
 
-    private static void usage() {
-    	System.out.println("Usage: hadoop jar hdfs.jar <cmd> [<arguments>");
-    	System.out.println("\thadoop jar hdfs.jar put source_file .");
-    	System.out.println("\thadoop jar hdfs.jar put source_file destination_file");
-    	System.out.println("\thadoop jar hdfs.jar put source_file /user/coudera/");
-    	System.out.println("\thadoop jar hdfs.jar put source_file /user/coudera/destination_file");
-    	System.out.println("\thadoop jar hdfs.jar put source_file hdfs://quickstart.cloudera:8020/user/cloudera/");
-    	System.out.println("\thadoop jar hdfs.jar put source_file hdfs://quickstart.cloudera:8020/user/cloudera/destination_file");
+  private static void usage() {
+    System.out.println("Usage: hadoop jar hdfs.jar <cmd> [<arguments>");
+    System.out.println("\thadoop jar hdfs.jar put source_file .");
+    System.out.println("\thadoop jar hdfs.jar put source_file destination_file");
+    System.out.println("\thadoop jar hdfs.jar put source_file /user/coudera/");
+    System.out.println("\thadoop jar hdfs.jar put source_file /user/coudera/destination_file");
+    System.out.println("\thadoop jar hdfs.jar put source_file hdfs://quickstart.cloudera:8020/user/cloudera/");
+    System.out.println("\thadoop jar hdfs.jar put source_file hdfs://quickstart.cloudera:8020/user/cloudera/destination_file");
 
-    	System.out.println("\n");
-    	System.out.println("\thadoop jar hdfs.jar get hdfs://localhost/users/coudera/file .");
-    }
-    
+    System.out.println("\n");
+    System.out.println("\thadoop jar hdfs.jar get hdfs://localhost/users/coudera/source_file .");
+    System.out.println("\thadoop jar hdfs.jar get hdfs://localhost/users/coudera/source_file destination_file");
+  }
 
-    public static void main(String[] args) throws IOException {
-	if (args.length == 0) {
-	    usage();
-	    return;
-	}
-	if (args.length == 3) {
-		switch (args[0]) {
-    	        case "put" :
-    		    command.put(args[1], args[2]);
-    		    break;
-		default:
-		    usage();
-		    break;
-	    }
-	}
+
+  public static void main(String[] args) throws Exception {
+    if (args.length == 0) {
+      usage();
+      return;
     }
+    if (args.length == 3) {
+      switch (args[0]) {
+        case "put" :
+        command.put(args[1], args[2]);
+        break;
+
+        case "get" :
+        command.get(args[1], args[2]);
+        break;
+
+        default:
+        usage();
+        break;
+      }
+    }
+  }
 }
