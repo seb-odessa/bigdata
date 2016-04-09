4212cd62f7c957eb3cffc2239ccc85fc28f68a49
seb-odessa <seb@ukr.net>
2016-04-09 16:05:40 +0300

    First successful build of lesson 3

--- Lesson  3/src/payments.java
+++ Lesson  3/src/payments.java
@@ -14,54 +14,60 @@ import java.math.BigDecimal;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Payments {
+public class payments {
 
   public static class PaymentMapper
-    extends Mapper<Object, Text, IntWritable, DoubleWritable>{
+          extends Mapper<Object, Text, IntWritable, DoubleWritable>{
     private IntWritable id = new IntWritable();
     private DoubleWritable payment = new DoubleWritable();
     private Pattern pattern =
-      Pattern.compile("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} (\\d+) ([\\d.]+)");
+            Pattern.compile("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2} (\\d+) ([\\d.]+)");
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-      Matcher matcher = linePattern.matcher(value.toString());
-      if (!matcher.find()) {
-          id.set(Integer.parseInt(matcher.group(1)));
-	  payment.set(new Double.parseDouble(matcher.group(2)));
-    	  context.write(id, payment);
-	}
+      Matcher matcher = pattern.matcher(value.toString());
+      if (matcher.find()) {
+        id.set(Integer.parseInt(matcher.group(1)));
+        payment.set(Double.parseDouble(matcher.group(2)));
+        context.write(id, payment);
+      }
     }
   }
 
   public static class PaymentReducer
-    extends Reducer<IntWritable, DoubleWritable, IntWritable, DoubleWritable> {
+          extends Reducer<IntWritable, DoubleWritable, IntWritable, DoubleWritable> {
     private IntWritable id = new IntWritable();
     private DoubleWritable payment = new DoubleWritable();
 
     public void reduce(IntWritable key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
-      Double sum = 0;
+      double sum = 0;
       for (DoubleWritable payment: values) {
-    	    sum = sum.add(payment.get());
+        sum += payment.get();
       }
-
-      paymentWritable.set(sum);
-
-      context.write(key, paymentWritable);
+      payment.set(sum);
+      context.write(key, payment);
     }
   }
 
+  private static void usage() {
+    System.out.println("Usage: hadoop jar payments.jar <input> <output>");
+    System.out.println("\thadoop jar payments.jar  /user/seb/payments.log .");
+  }
   public static void main(String[] args) throws Exception {
+    if (args.length != 2) {
+      usage();
+      return;
+    }
     Configuration conf = new Configuration();
 
     Job job = Job.getInstance(conf, "Customer payments");
-    job.setJarByClass(CustomerPaymentsJob.class);
+    job.setJarByClass(payments.class);
 
-    job.setMapperClass(PaymentLineMapper.class);
-    job.setCombinerClass(UserPaymentsReducer.class);
-    job.setReducerClass(UserPaymentsReducer.class);
+    job.setMapperClass(PaymentMapper.class);
+    job.setCombinerClass(PaymentReducer.class);
+    job.setReducerClass(PaymentReducer.class);
 
     job.setOutputKeyClass(IntWritable.class);
-    job.setOutputValueClass(BigDecimalWritable.class);
+    job.setOutputValueClass(DoubleWritable.class);
 
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
--- README.md
+++ README.md
@@ -18,6 +18,32 @@ Usage:
 	hadoop jar hdfs.jar get hdfs://localhost/users/coudera/source_file .
 	hadoop jar hdfs.jar get hdfs://localhost/users/coudera/source_file destination_file
 
+#Lesson 3. MapReduce API
+
+	$ hdfs dfsadmin -printTopology
+	16/04/09 12:21:09 WARN util.NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
+	Rack: /default-rack
+	   10.0.3.30:50010 (vm0)
+	   10.0.3.31:50010 (vm1)
+	   10.0.3.32:50010 (vm2)
+
+	$ hdfs dfs -put Lessons/bigdata-session-3/datasets/payments.log /user/seb
+	$ hdfs dfs -put Lessons/bigdata-session-3/datasets/facebook_combined.txt /user/seb
+	$ hdfs dfs -ls /user/seb
+	Found 2 items
+	-rw-r--r--   3 seb supergroup     854362 2016-04-09 12:29 /user/seb/facebook_combined.txt
+	-rw-r--r--   3 seb supergroup        436 2016-04-09 12:28 /user/seb/payments.log
+	hdfs dfs -cat /user/seb/payments.log
+	2014-07-02 20:52:39 1 12.01 www.store1.com
+	2014-07-02 20:52:39 1123 1.75 www.store1.com
+	2014-07-02 20:52:39 12 4.05 www.store2.com
+	2014-07-02 20:52:39 1 7.87 www.store1.com
+	2014-07-02 20:52:40 12 124.67 www.store2.com
+	2014-07-02 20:52:40 1 9.14 www.store3.com
+	2014-07-02 20:52:40 1123 14.75 www.store1.com
+	2014-07-02 20:52:40 12 54.95 www.store2.com
+	2014-07-02 20:52:40 1 77.70 www.store3.com
+	2014-07-02 20:52:40 12 1.99 www.store4.com
 
 
 # Links and tips
